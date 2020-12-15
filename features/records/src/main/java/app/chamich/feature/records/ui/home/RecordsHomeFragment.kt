dc8e8315f24e7246193db5226c8a7ecffb8d7072
Viktor Apoyan <chamich.apps@gmail.com>
2020-12-15 21:16:40 +0100

    Added Entry point fragment for records feature and initial navigation graph

--- features/records/src/main/java/app/chamich/feature/records/ui/home/RecordsHomeFragment.kt
+++ features/records/src/main/java/app/chamich/feature/records/ui/home/RecordsHomeFragment.kt
@@ -14,10 +14,27 @@
  *  imitations under the License.
  */
 
-plugins {
-    id("commons.android-feature")
-}
+package app.chamich.feature.records.ui.home
 
-dependencies {
-    implementation(project(BuildModules.Libraries.DESIGN))
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import app.chamich.feature.records.R
+
+
+/**
+ * Records Home Fragment
+ * Entry point for navigation through the records.
+ */
+class RecordsHomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_records_home, container, false)
+    }
+
 }
